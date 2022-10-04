9fd7ae8320837729bc2df288d021c3685f07c021
ksllove <111697142+ksllove@users.noreply.github.com>
2022-10-05 01:10:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olfxQ58MMgO5QMdD36mn_k1nzhl4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'GLryK9cC7NMI4uxu1WNacdT1s2t9e0IMxIi6NsRUM28',
+      useTemplateId: 'bafctvK546wtgpZq58aHz-9PzVCx6WIRMvL2aTjpXcw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-17',
       festivals: [
